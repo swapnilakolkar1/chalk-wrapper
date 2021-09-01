b61d7cc9c2ae8f5d090619ee78bc604f335c1f55
Swapnil Akolkar <swapnil.akolkar1@gmail.com>
2021-09-01 20:28:21 +0530

    bug fix 1.0.1

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 const chalk = require('chalk');
 
-console.log(chalk.blue('Hello world!'));
-console.log(chalk.red('Hello world!'));
-console.log(chalk.yellow('Hello world!'));
 function ChalkWrapper(string, color) {
     if (string && color) {
         switch (color) {
-            case 'blue': console.log(chalk.blue(string)); break;
-            case 'red': console.log(chalk.red(string)); break;
-            case 'yellow': console.log(chalk.yellow(string)); break;
+            case 'blue': { console.log(chalk.blue(string)); break };
+            case 'red': { console.log(chalk.red(string)); break };
+            case 'yellow': { console.log(chalk.yellow(string)); break };
 
         }
     }
